0ab16e8888f71b47b279dc7a2ddf553099b0b0da
enumm <weliukis@gmail.com>
2015-05-08 00:56:42 -0700

    update

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,6 +1,11 @@
 (function() {
     //kazkaip atrodo hack
 
+    Races = {
+    	Plebs: {name :'Plebs', buildings: ['PlebHut, OtherHut']},
+    	BlaBlas: {name :'BlaBlas', buildings: ['PlebHut, OtherHut']}
+    };
+
 	UnitTypes = {
     	Pleb: {name: 'Pleb', type: 'ground', damage: 10, armor: 10, movementSpeed: 60, life: 100, attackSpeed: 1, range: 10},
     	RangedPleb: {name: 'RangedPleb', type: 'ranged', damage: 8, armor: 5, movementSpeed: 60, life: 100, attackSpeed: 2, range: 100}
