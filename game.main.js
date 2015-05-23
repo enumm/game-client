2f038576e1a3246a175a87ea5df8c79bb38300c3
Zverutis <skarbonke3@gmail.com>
2015-05-23 16:37:47 +0300

    test for zoom disable

--- game.main.js
+++ game.main.js
@@ -558,59 +558,6 @@ function handleComplete(event) {
 
     render_google_btn();
 
-	var page = document.getElementById('canvasHolder');
-    var ua = navigator.userAgent;
-	var iphone = ~ua.indexOf('iPhone') || ~ua.indexOf('iPod');
-	var ipad = ~ua.indexOf('iPad');
-	var ios = iphone || ipad;
-	    // Detect if this is running as a fullscreen app from the homescreen
-	var fullscreen = window.navigator.standalon;
-	var android = ~ua.indexOf('Android');
-	var lastWidth = 0;
-	 
-	if (android) {
-	  // Android's browser adds the scroll position to the innerHeight, just to
-	  // make this really fucking difficult. Thus, once we are scrolled, the
-	  // page height value needs to be corrected in case the page is loaded
-	  // when already scrolled down. The pageYOffset is of no use, since it always
-	  // returns 0 while the address bar is displayed.
-	  window.onscroll = function() {
-	  	
-	    document.body.style.height = window.innerHeight + 'px'
-	  } 
-	}
-	var setupScroll = window.onload = function() {
-		var page = document.getElementById('canvasHolder');
-	  // Start out by adding the height of the location bar to the width, so that
-	  // we can scroll past it
-	  if (ios) {
-	    // iOS reliably returns the innerWindow size for documentElement.clientHeight
-	    // but window.innerHeight is sometimes the wrong value after rotating
-	    // the orientation
-	    var height = document.documentElement.clientHeight;
-	    // Only add extra padding to the height on iphone / ipod, since the ipad
-	    // browser doesn't scroll off the location bar.
-	    if (iphone && !fullscreen) height += 60;
-	    document.body.style.height = height + 'px';
-	  } else if (android) {
-	    // The stock Android browser has a location bar height of 56 pixels, but
-	    // this very likely could be broken in other Android browsers.
-	    document.body.style.height = (window.innerHeight + 56) + 'px'
-	  }
-	  // Scroll after a timeout, since iOS will scroll to the top of the page
-	  // after it fires the onload event
-	  setTimeout(scrollTo, 0, 0, 1);
-	};
-	(window.onresize = function() {
-		var page = document.getElementById('canvasHolder');
-	  var pageWidth = page.offsetWidth;
-	  // Android doesn't support orientation change, so check for when the width
-	  // changes to figure out when the orientation changes
-	  if (lastWidth == pageWidth) return;
-	  lastWidth = pageWidth;
-	  setupScroll();
-	})();
-
     OnResizeCalled();
 }
 
